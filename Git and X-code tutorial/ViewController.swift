dbd45a6e9e56f5705c6f940895665dc6ea80c208
Danton Corbel <dantoncorbel@gmail.com>
2019-04-15 08:54:57 +1200

    Initial Commit

---
+++ Git and X-code tutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Git and X-code tutorial
+//
+//  Created by Deb Santa Clara on 15/04/19.
+//  Copyright © 2019 Deb Santa Clara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
